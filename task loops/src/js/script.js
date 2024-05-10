6767b1a48a6cb0df45ef5578382ed6102b121d46
Mirze26 <mirzaab@code.edu.az>
2024-05-10 12:51:48 +0400

    for loop task

---
+++ task loops/src/js/script.js
@@ -0,0 +1,68 @@
+
+
+// Task 1......................................
+
+// let arr = [6, 7, 8, 15, 16, 17];
+
+// console.log(arr);
+
+// console.log("")
+
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     if (arr[i] % 2 == 1) {
+//         arr[i]++;
+//     }
+// }
+
+// console.log(arr);
+
+//----------------------------------------------
+
+
+
+
+
+
+//Task 2........................................
+
+// let arr = [8, 10, 19, 6, 26];
+
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     if (arr[i] % 2 != 0) {
+//         let lastElement = arr[arr.length - 1]
+//         console.log(arr[i] * lastElement);
+//     }
+
+//     else {
+//         console.log(arr[i] * arr[0]);
+//     }
+// }
+
+//--------------------------------------------------
+
+
+
+
+
+
+
+//Task3.............................................
+
+// let arr = [8, 10, 19, 6, 26];
+// let min = arr[0];
+// let max = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (max > arr[i]) {
+//         max = arr[i];
+//     }
+//     else if (min < arr[i]) {
+//         min = arr[i]
+//     }
+// }
+
+// console.log(max * min)
+
+//----------------------------------------------------
+
+
